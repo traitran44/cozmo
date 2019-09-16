1736d0a24daaba35a631ed503e23dbd0dc809c9d
ttran96 <ttran96@gatech.edu>
2019-09-15 23:19:37 -0400

    Completed Project 1

--- lab2/fsm.py
+++ lab2/fsm.py
@@ -51,18 +51,17 @@ class Order(State):
                                   num_retries=0).wait_for_completed()
         self.robot.drive_wheels(l_wheel_speed=12 * math.pi, r_wheel_speed=50 * math.pi, duration=5)
 
-        while True:
-            pass
+        return self.next_state()
 
     def next_state(self):
-        return None
+        return Idle()
 
 
 class Inspection(State):
     def __init__(self):
         self.observed_symbol = None
         self.robot = None
-        print("Order State")
+        print("Inspection State")
 
     def run(self, sdk_conn):
         self.robot = sdk_conn.wait_for_robot()
@@ -90,31 +89,31 @@ class Inspection(State):
         for i in range(4):
             height = 1.0 if lift_height else 0.0
             lift_speed = math.pi / 15 if lift_height else -math.pi / 15
-            print("Height: ", height)
             self.robot.set_lift_height(height=height, in_parallel=True, num_retries=0)
             self.robot.move_lift(speed=lift_speed)
-            drive_action = self.robot.drive_straight(cozmo.util.distance_mm(distance),
-                                                     cozmo.util.speed_mmps(distance / drive_duration),
-                                                     should_play_anim=True,
-                                                     in_parallel=True,
-                                                     num_retries=0).wait_for_completed()
-            turn_action = self.robot.turn_in_place(cozmo.util.degrees(angle), in_parallel=True,
-                                                   num_retries=0, speed=cozmo.util.degrees(angle / rotate_duration),
-                                                   accel=None, angle_tolerance=None,
-                                                   is_absolute=False).wait_for_completed()
+            self.robot.drive_straight(cozmo.util.distance_mm(distance),
+                                      cozmo.util.speed_mmps(distance / drive_duration),
+                                      should_play_anim=True,
+                                      in_parallel=True,
+                                      num_retries=0).wait_for_completed()
+            self.robot.turn_in_place(cozmo.util.degrees(angle), in_parallel=True,
+                                     num_retries=0, speed=cozmo.util.degrees(angle / rotate_duration),
+                                     accel=None, angle_tolerance=None,
+                                     is_absolute=False).wait_for_completed()
             lift_height = not lift_height
 
         self.robot.backup_onto_charger(max_drive_time=10)
+        return self.next_state()
 
     def next_state(self):
-        return None
+        return Idle()
 
 
 class Idle(State):
     def __init__(self):
         self.observed_symbol = None
         self.label = None
-        print("idle_state")
+        print("State: Idle")
 
     def run(self, sdk_conn):
         robot = sdk_conn.wait_for_robot()
@@ -140,55 +139,72 @@ class Idle(State):
 
                 if not next is None:
                     return next
-
-        """
-        Go to next states
-        """
+        return None
 
     def next_state(self):
         if self.label == "drone":
             return Drone()
-        return None
+        elif self.label == "order":
+            return Order()
+        elif self.label == "inspection":
+            return Inspection()
+        return Idle()
 
 
 class Drone(State):
     def __init__(self):
+        print("State: Drone")
+        self.done = False
         self.robot = None
         self.picking_up = False
+        self.action: cozmo.action.Action = None
 
-    def object_handler(self, *args, **kwargs):
-        # for arg in args:
-        #     print(arg)
-        # for key, val in kwargs.items():
-        #     print(key, ": ", val)
-        # self.robot.stop_all_motors().wait_for_completed()
+    async def object_handler(self, *args, **kwargs):
         if not self.picking_up:
-            print(kwargs['obj'])
             self.picking_up = True
-            self.robot.dock_with_cube(kwargs['obj'],
-                                      alignment_type=cozmo.robot_alignment.RobotAlignmentTypes.LiftFinger,
-                                      in_parallel=False,
-                                      num_retries=3).wait_for_completed()
-            self.robot.set_lift_height(height=1, in_parallel=True, num_retries=1).wait_for_completed()
-            self.robot.move_lift(math.pi / 4)
+
+            lift_height = self.robot.lift_position.ratio
+            if lift_height > 0:
+                await self.robot.set_lift_height(height=0.0, in_parallel=True, num_retries=2).wait_for_completed()
+
+            self.robot.stop_all_motors()
+            await self.robot.dock_with_cube(kwargs['obj'],
+                                            alignment_type=cozmo.robot_alignment.RobotAlignmentTypes.LiftFinger,
+                                            in_parallel=True, num_retries=2).wait_for_completed()
+
+            await self.robot.set_lift_height(height=1.0, in_parallel=True, num_retries=1).wait_for_completed()
+
+            distance = 100
+            drive_duration = 1
+            await self.robot.drive_straight(cozmo.util.distance_mm(distance),
+                                            cozmo.util.speed_mmps(distance / drive_duration),
+                                            should_play_anim=True,
+                                            in_parallel=True,
+                                            num_retries=0).wait_for_completed()
+
+            await self.robot.set_lift_height(height=0.0, in_parallel=True, num_retries=1).wait_for_completed()
+
+            distance = -100
+            drive_duration = 1
+            await self.robot.drive_straight(cozmo.util.distance_mm(distance),
+                                            cozmo.util.speed_mmps(distance / drive_duration),
+                                            should_play_anim=True,
+                                            in_parallel=True,
+                                            num_retries=0).wait_for_completed()
+            self.done = True
+
 
     def run(self, sdk_conn):
-        """
-        TODO HERE
-        """
         self.robot = sdk_conn.wait_for_robot()
         self.robot.camera.image_stream_enabled = True
         self.robot.camera.color_image_enabled = False
         self.robot.camera.enable_auto_exposure()
         self.robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
-
-        self.robot.say_text("drone state").wait_for_completed()
-
-        self.robot.set_lift_height(height=0, in_parallel=False, num_retries=0).wait_for_completed()
-        self.robot.move_lift(speed=-math.pi)
+        # self.robot.say_text("drone state").wait_for_completed()
 
         distance = 200
-        drive_duration = 2
+        drive_duration = 1
+
         self.robot.drive_straight(cozmo.util.distance_mm(distance), cozmo.util.speed_mmps(distance / drive_duration),
                                   should_play_anim=True,
                                   in_parallel=False,
@@ -202,10 +218,11 @@ class Drone(State):
 
         self.robot.add_event_handler(cozmo.objects.EvtObjectObserved, self.object_handler)
 
-        while True:
+        while not self.done:
             pass
 
-        # return self.next_state()
+        return self.next_state()
+
 
     def next_state(self):
         return Idle()
@@ -217,12 +234,12 @@ class FSM():
 
     def run(self, sdk_conn):
         self.sdk_conn = sdk_conn
-        # while not self.state is None:
-        self.state = self.state.run(sdk_conn=self.sdk_conn)
+        while not self.state is None:
+            self.state = self.state.run(sdk_conn=self.sdk_conn)
 
 
 if __name__ == "__main__":
-    start_state = Drone()
+    start_state = Idle()
     fsm = FSM(start_state=start_state)
     cozmo.setup_basic_logging()
     try:
