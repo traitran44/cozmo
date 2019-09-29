0442704a14abf8b28dcc50c90096f9376c6f8881
ttran96 <ttran96@gatech.edu>
2019-09-29 15:10:53 -0400

    Update particle_filter

--- lab3/particle_filter.py
+++ lab3/particle_filter.py
@@ -4,6 +4,7 @@ from utils import *
 import setting
 import numpy as np
 import random
+
 np.random.seed(setting.RANDOM_SEED)
 random.seed(setting.RANDOM_SEED)
 
@@ -27,6 +28,7 @@ def motion_update(particles, odom):
         motion_particles[i].y += dy
         motion_particles[i].h = motion_particles[i].h + odom[2]
     return motion_particles
+
 
 # ------------------------------------------------------------------------
 def measurement_update(particles, measured_marker_list, grid):
@@ -60,15 +62,36 @@ def measurement_update(particles, measured_marker_list, grid):
         weights[i] = weight_update(pairs)
 
     weights = normalize_weights(weights)
-    measured_particles = resample(particles, weights)
+    measured_particles = resample(particles, weights, len(particles))
 
     return measured_particles
 
 
 # Resample new particles based on weights
-def resample(particles, weights):
+def resample(particles, weights, new_size):  # Trai
+    """
+    :param particles: [ (x, y, heading), ...., (..) ]
+    :param weights: [0.2, 0.4, 0.7, 1.0]
+    :return:  new particles [ (x, y, heading), ...., (..) ]
+    """
     new_particles = []
+    # Prefix sum prob, setup probability range for each particle
+    for i in range(1, len(weights)):
+        weights[i] = weights[i - 1] + weights[i]
+
+    for i in range(new_size):
+        rand = random.random()
+        for i in range(len(weights)):
+            if rand <= weights[i]:
+                chosen = particles[i]
+                new_particles.append(chosen)
+                break
+
+    if len(new_particles) < new_size:
+        print("New particles size cannot be less than particles size!")
+
     return new_particles
+
 
 # Pair each marker to its corresponding match (with noise)
 def get_match_markers(robot_markers, particle_markers):
@@ -77,8 +100,13 @@ def get_match_markers(robot_markers, particle_markers):
 
 
 # Normalize s.t sum to 1
-def normalize_weights(weights):
+def normalize_weights(weights):  # Trai
+    total = sum(weights)
     norm_w = []
+    for w in weights:
+        norm_w.append(
+            w / total
+        )
     return norm_w
 
 
@@ -86,3 +114,27 @@ def normalize_weights(weights):
 def weight_update(pairs):
     w = 1
     return w
+
+
+if __name__ == "__main__":
+    # test resample
+    new_p = resample(
+        [0, 1, 2, 4],
+        [0.1, 0.2, 0.4, 0.3],
+        1000000
+    )
+    freq = {}
+    for p in new_p:
+        if p not in freq:
+            freq[p] = 0
+        freq[p] += 1
+
+    for k in freq.keys():
+        freq[k] = freq[k] / 1000000
+        print("Freq ", k, " is ", freq[k])
+
+    # test normalize_weights
+    norm_w = normalize_weights(
+        [2, 3, 10, 22, 32, 1]
+    )
+    print("Sum Norm weight: ", sum(norm_w))
